f8d4ec3ec98e6961680990032e43f8a4c0afc4f0
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-05-15 14:09:08 +0300

    bulk commit

---
+++ src/app/highlightt.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightDirective } from './highlight.directive';
+
+describe('HighlighttDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
